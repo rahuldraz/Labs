f04e8b672e6cd8f63d237ee248285c27e08b9640
rahuldraz <rahullraz@gmail.com>
2020-12-17 01:28:29 +0530

    CP Lab Updated

---
+++ 1st-Year/C-programming/Q41.c
@@ -0,0 +1,24 @@
+//WAP in C to condense number into single digit
+#include <stdio.h>
+
+
+int main()
+{
+    int n,sum=0;
+
+
+    printf("Enter number to condense: ");
+    scanf("%d", &n);
+    
+    while(n > 0 || sum > 9) 
+    { 
+        if(n == 0) 
+        { 
+            n = sum; 
+            sum = 0; 
+        } 
+        sum += n % 10; 
+        n /= 10; 
+    } 
+    printf("The condense Sum ->%d\n",sum);
+}
---
+++ 1st-Year/C-programming/Q43.c
@@ -0,0 +1,27 @@
+//WAP in C to find fibannaci series upto Nth term using Loop
+#include <stdio.h>
+
+
+int main()
+{
+    int n;
+
+    //Magic Initialization
+    int a = 0,b = 1,c = 0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+    
+
+    printf("Fibonacci terms: \n");
+
+    
+    for(int i=0; i<n; i++)
+    {
+        printf("%d, ", c);
+        a = b;     
+        b = c;   
+        c = a + b;
+    } 
+
+}
